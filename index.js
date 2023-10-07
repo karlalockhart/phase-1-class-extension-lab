1251a1397e65b9cafcaa34a3b065f9281b80c2cf
Karla Lockhart <kblockhart@gmail.com>
2023-10-07 09:22:15 -0700

    made changes to index.js & completed phase-1-class-extension-lab

--- index.js
+++ index.js
@@ -1,1 +1,79 @@
 // Your code here
+/*
+class Polygon {
+    [0,1,2,3,4];
+    get countSides(){
+
+    }
+    get perimeter(){
+
+    }
+}
+
+class Triangle extends Polygon{
+    get isValid(){
+
+    }
+}
+
+class Square extends Polygon(
+    get isValid(){
+
+    }
+    get area(){
+
+    }
+)
+*/
+
+class Polygon {
+    constructor(sides) {
+      this.sides = sides
+      this.count = this.sides.length
+    }
+  
+    get countSides() {
+      return this.sides.length
+    }
+  
+    get perimeter() {
+      if (!Array.isArray(this.sides)) return;
+      let sum = 0;
+      for (var int of this.sides) {
+        sum += int
+      }
+      return sum
+    }
+  }
+  
+  class Triangle extends Polygon {
+  
+    get isValid() {
+      if (!Array.isArray(this.sides)) return;
+      if (this.count !== 3) return;
+      let side1 = this.sides[0]
+      let side2 = this.sides[1]
+      let side3 = this.sides[2]
+      return ((side1 + side2 > side3) && (side1 + side3 > side2) && (side2 + side3 > side1))
+    }
+  }
+  
+  class Square extends Polygon {
+  
+    get isValid() {
+      if (!Array.isArray(this.sides)) return;
+      if (this.count !== 4) return;
+      let side1 = this.sides[0]
+      let side2 = this.sides[1]
+      let side3 = this.sides[2]
+      let side4 = this.sides[3]
+      return ((side1 === side2) && (side1 === side3) && (side1 === side4))
+    }
+  
+    get area() {
+      if (!Array.isArray(this.sides)) return;
+      if (this.count !== 4) return;
+      return this.sides[0] * this.sides[0]
+    }
+  }
+  
